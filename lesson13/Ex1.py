d32be2d5b87fd24521984f904a6010ec1bd220b0
rubaqeshta04 <qeshtaruba@gmail.com>
2026-07-01 15:28:09 +0300

    Solve Lecture 13 exercises

---
+++ lesson13/Ex1.py
@@ -0,0 +1,9 @@
+product = {
+    "name": "Laptop",
+    "price": 1200,
+    "quantity": 5
+}
+
+discount = product.get("discount", 0)
+
+print(discount)
---
+++ lesson13/Ex3.py
@@ -0,0 +1,5 @@
+emails = ["a@gmail.com", "b@gmail.com", "a@gmail.com", "c@gmail.com"]
+
+unique_emails = set(emails)
+
+print(len(unique_emails))
